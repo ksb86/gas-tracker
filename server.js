dbdc874154469f40e03937928982fe3132bf32b6
Shane Barnwell <shane.barnwell@.overstock.com>
2017-07-24 01:51:38 -0600

    favicon stuff

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var api = require('./serverRoutes/api');
 var app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
