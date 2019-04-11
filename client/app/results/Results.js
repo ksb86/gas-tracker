70085f9140b5276c434614da795b3b36e4961f03
Shane Barnwell <shane.barnwell@overstock.com>
2019-04-10 23:46:01 -0600

    window

--- client/app/results/Results.js
+++ client/app/results/Results.js
@@ -6,19 +6,19 @@ class Results extends React.Component {
 
     constructor(props) {
         super(props);
-    }
-    onSignIn = googleUser => {
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+        window.onSignIn = googleUser => {
+            var profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+        }
     }
       
     render() {
         return (
             <div>
-                <div className="g-signin2" data-onsuccess={this.onSignIn}></div>
+                <div className="g-signin2" data-onsuccess="onSignIn"></div>
                 <div className={`entry-result ${(this.props.state.form.entrySuccess) ? 'success' : ''}`}>
                     {this.props.state.form.entrySuccess &&
                         <div className="entry-success-message">Entry was added!</div>}
